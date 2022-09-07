5734fc96bf974eac9b397a792aba4a3ded3c5e07
collinradichel <radichel.collin@gmail.com>
2022-09-07 15:57:35 -0500

    started adding event listeners for movement of player

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ class Sprite {
     }
     update(){
         this.draw()
+
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height){
@@ -57,16 +59,57 @@ enemy.draw();
 
 console.log(player)
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle= 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x=0;
+
+    if(keys.a.pressed && lastKey === 'a'){
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate();
 
 window.addEventListener('keydown', (event) => {
-console.log(event)
+    switch(event.key){
+        case 'd':
+        keys.d.pressed = true
+        lastKey = 'd'
+         break
+         case 'a':
+         keys.a.pressed= true
+         lastKey = 'a'
+         break
+    }
+console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key){
+        case 'd':
+        keys.d.pressed = false
+         break
+         case 'a':
+        keys.a.pressed = false
+         break
+    }
+console.log(event.key)
 })
