39cc8462dc1eb0365216392eaa0bee9afffbaec2
collinradichel <radichel.collin@gmail.com>
2022-09-15 10:58:59 -0500

    attacks for player 1 and 2 added, space for player 1 attack, ArrowDown for player 2 attack

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ c.fillRect(0,0,canvas.width,canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity, color = 'red'}) {
+    constructor({position, velocity, color = 'red', offset}) {
      this.position = position
      this.velocity = velocity
      this.width = 50
@@ -20,6 +20,7 @@ class Sprite {
             x: this.position.x,
             y: this.position.y
         },
+        offset,
         width: 100,
         height: 50
      }
@@ -44,7 +45,7 @@ class Sprite {
     }
     update(){
         this.draw()
-        this.attackBox.position.x = this.position.x
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
@@ -69,7 +70,11 @@ const player = new Sprite({
     },
     velocity: {
         x: 0,
-        y: 10
+        y: 0
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 })
 
@@ -82,7 +87,11 @@ const enemy = new Sprite({
         x: 0,
         y: 0
     },
-    color: 'blue'
+    color: 'blue',
+    offset: {
+        x: -50,
+        y: 0
+    }
 })
 
 player.draw();
@@ -110,6 +119,16 @@ const keys = {
 
 let lastKey
 
+function rectangularCollision({rectangle1, rectangle2}) {
+ return(
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+    rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+    && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+ )
+}
+
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle= 'black'
@@ -135,15 +154,29 @@ function animate() {
     }
 
     //detect for collision
-    if(player.attackBox.position.x + player.attackBox.width >=
-         enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width &&
-         player.attackBox.position.y + player.attackBox.height >= enemy.position.y
-         && player.attackBox.position.y <= enemy.position.y + enemy.height &&
+    if(
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
          player.isAttacking
          ) {
+        player.isAttacking = false
         console.log('HIT!')
+         }
+
+    if(
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+            enemy.isAttacking
+            ) {
+        enemy.isAttacking = false
+        console.log('enemy attack successful')
     }
 }
+
 
 animate();
 
@@ -176,8 +209,10 @@ window.addEventListener('keydown', (event) => {
          case 'ArrowUp':
          enemy.velocity.y = -20
          break
+         case 'ArrowDown':
+         enemy.isAttacking = true
+         break
     }
-console.log(event.key)
 })
 
 window.addEventListener('keyup', (event) => {
@@ -199,6 +234,4 @@ window.addEventListener('keyup', (event) => {
         keys.ArrowLeft.pressed = false
          break
     }
-
-console.log(event.key)
 })
